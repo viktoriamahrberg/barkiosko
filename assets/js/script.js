de7b1792d904a0f85944ab1b5d2d5b3ad6aa5a0e
Vikmah <viktoria.mahrberg@gmail.com>
2022-12-08 11:00:15 +0000

    Add zoom effect on Opening Soon block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$('.my-background-video').bgVideo();
 });
+
+// $(document).scroll(function () {
+//     if (window.scrollY > 50) {
+//         $(".zoom").stop().animate({
+//             fontSize: '17px'
+//         });
+//     } else {
+//         $(".zoom").stop().animate({
+//             fontSize: '25px'
+//         });
+// 	console.log('zoom');
+//     }
+// });
+
+$(function() {
+
+	$(window).scroll(function() {
+  
+	  var mass = Math.min(5, 1+0.002*$(this).scrollTop());
+  
+	  $('#zoom').css('transform', 'scale(' + mass + ')');
+	});
+  });
