3fdcc1646db61aac59a1130b14e1ceae3a2a7339
Vikmah <viktoria.mahrberg@gmail.com>
2022-12-08 12:04:01 +0000

    Fix Opening Soon on laptop size

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,15 @@ $(document).ready(function () {
 	$('.my-background-video').bgVideo();
 });
 
-// $(document).scroll(function () {
-//     if (window.scrollY > 50) {
-//         $(".zoom").stop().animate({
-//             fontSize: '17px'
-//         });
-//     } else {
-//         $(".zoom").stop().animate({
-//             fontSize: '25px'
-//         });
-// 	console.log('zoom');
-//     }
-// });
+
+// https://codepen.io/seriouslyally/pen/XwKMzj
 
 $(function() {
 
 	$(window).scroll(function() {
   
-	  var mass = Math.min(5, 1+0.002*$(this).scrollTop());
+	  var mass = Math.min(10, 1+0.002*$(this).scrollTop());
   
-	  $('#zoom').css('transform', 'scale(' + mass + ')');
+	  $('#zoom-content').css('transform', 'scale(' + mass + ')');
 	});
   });
